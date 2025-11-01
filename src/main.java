af0c780fccb58d306dccfb4fd76d3996e02d4e9d
Elhadj Bah <issiagabahh1997@icloud.com>
2025-11-01 14:29:37 -0600

    Drafting ideas.

--- src/main.java
+++ src/main.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
-
 public class main {
+
+
 }
+/*
+
+The Budget we need to have different users login in for each person in the house hoousehold.
+
+Each user should have limited privilege but the admin can acces the whole systems and only he can edit and view
+all of the transcations.
+
+All users should be able to add their expenses with features that add details regarding the expense and add catogory.
+and how much mmoney they have.
+
+As a collective systems we should have an options where can see the average spend and the average that's added each month or
+weeklly and future planning.
+
+The data stuctures that much be used is Arraylist and Stacks and queues and listIterate and maybe implement linkedlist.
+
+ */
